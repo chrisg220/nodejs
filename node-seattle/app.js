32f108a4f0727094fc554a8d2c9e1319b17397d6
Christopher Gonzales <chrisg220@hotmail.com>
2013-03-30 11:41:43 -0700

    add basic chat client

---
+++ node-seattle/app.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    socketio = require('socket.io');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/', function(req, res) {
+  res.sendfile('./public/html/index.html');
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = socketio.listen(server);
+
+io.sockets.on('connection', function (socket) {
+  console.log('client connected');
+
+  socket.on('message', function(message) {
+    io.sockets.emit('message', message);
+  });
+
+});
